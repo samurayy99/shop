e61d2ba2e787cb2e4fd7b7f9f1158d1d7539ef6b
samurayy99 <130201158+samurayy99@users.noreply.github.com>
2023-10-11 03:04:29 +0200

    Neubegin17

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Welcome to the Webshop</h1>
+  </template>
+  
+  <script>
+  export default {
+    props: ['foo']
+  }
+  </script>
+  
--- resources/views/experimental/base.blade.php
+++ resources/views/experimental/base.blade.php
@@ -13,7 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <title>Highsociety > @yield('title')</title>
+    <title>{{ App\Models\Settings::get('app.name', 'Highsociety') }} > @yield('title')</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/img/favicon.ico') }}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
         rel="stylesheet">
